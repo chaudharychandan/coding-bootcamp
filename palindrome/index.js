d65dafacc1d002265058bbc32fed25f5cc770a6b
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-29 00:56:28 +0530

    Write a program which validates if the passed string is palindrone or not

---
+++ palindrome/index.js
@@ -0,0 +1,16 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+const reversestring = require('../reversestring/index');
+
+function palindrome(str) {
+  return str === reversestring(str);
+}
+
+module.exports = palindrome;
