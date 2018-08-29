6e51e97b3060ffce2202dad6f9417d73ad2620c1
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-30 02:44:21 +0530

    Create a pyramid of n levels

---
+++ pyramid/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+function pyramid(n) {
+  const cols = (n * 2) - 1;
+  const mid = Math.floor(cols / 2);
+  for (let row = 0; row < n; row++) {
+    let step = '';
+    for (let col = 0; col < cols; col++) {
+      if (mid-row <= col && mid+row >= col) {
+        step += '#';
+      } else {
+        step += ' ';
+      }
+    }
+    console.log(step);
+  }
+}
+
+module.exports = pyramid;
